a21c1574612752b83d28a3ea57cdca06d1751aa8
JeffKolnick <kolnickjeff@gmail.com>
2023-12-16 10:37:54 -0500

    Board state initalized and check for winning state

--- tictactoe.py
+++ tictactoe.py
@@ -21,5 +21,26 @@ def player_choice():
         
     return player_input
 
-first_or_second = player_choice()
+first_or_second = player_choice()           #Tracks if player is X's or O's
+board_state = [[' '] * 3 for _ in range(3)] #Track board state, initialized empty
 
+def three_in_a_row(X_or_O):
+
+    #Checks each row, column and diagonal for three of the same symbol,
+    #which is the win condition and returns True if so
+
+    #Check if any row contains the same symbol in all cells
+    for row in board_state:
+        if all(cell == X_or_O for cell in row):
+            return True
+        
+    #Check if any column contains the same symbol in all cells
+    for col in range(3):
+        if all(board_state[row][col] == X_or_O for row in range(3)):
+            return True   
+
+    #Check if any diagonal contains the same symbol in all cells
+    if all(board_state[i][i] == X_or_O for i in range(3)) or all(board_state[i][2 - i] == X_or_O for i in range(3)):
+        return True
+    
+    return False
