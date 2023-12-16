4177d3784865333c9cd215e7db37827dda1740f1
JeffKolnick <kolnickjeff@gmail.com>
2023-12-16 07:45:17 -0500

    Board drawing function

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+
+def draw_board():
+
+    # Takes the inputs for number of rows and columns
+    # (int, int) and prints the board accordingly
+
+    print("__|___|__")
+    print("__|___|__")
+    print("  |   |  ")
+  
+
+draw_board()
