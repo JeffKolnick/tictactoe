70e53110c4ef663428c8df89be6a16e442076081
JeffKolnick <kolnickjeff@gmail.com>
2023-12-16 08:11:31 -0500

    Requests player to decide which position they wish to play until valid choice is made

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,25 @@
 
-def draw_board():    
+def draw_board():  
+
+    #Draws the empty board  
 
     print("__|___|__")
     print("__|___|__")
     print("  |   |  ")
   
-
 draw_board()
+
+def player_choice():
+
+    #Asks player whether they want to go first (X's) or second (O's)
+    #and returns that choice
+
+    player_input = ""
+
+    while player_input not in ['X', 'O']:
+        player_input = input("Would you like to go first (X) or second (O)?: ")
+        
+    return player_input
+
+first_or_second = player_choice()
+
