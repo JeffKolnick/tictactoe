a062d600ff1570882d3dd8f4c9b34f4ea47aa017
JeffKolnick <kolnickjeff@gmail.com>
2023-12-17 07:02:35 -0500

    Modified draw_board function to be more robust. Also take player move input and validate legality

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,17 @@
 
-def draw_board():  
+board_state = [['_'] * 3 for _ in range(3)] #Track board state, initialized empty
 
-    #Draws the empty board  
+def draw_board(board_state):  
 
-    print("__|___|__")
-    print("__|___|__")
-    print("  |   |  ")
+    #Draws the current state of the board  
+
+    print("_{}_|_{}_|_{}_".format(board_state[0][0], board_state[0][1], board_state[0][2]))
+    print("_{}_|_{}_|_{}_".format(board_state[1][0], board_state[1][1], board_state[1][2]))
+    print(" {} | {} | {} ".format(' ' if board_state[2][0] == '_' else board_state[2][0],\
+                                  ' ' if board_state[2][1] == '_' else board_state[2][1],\
+                                  ' ' if board_state[2][2] == '_' else board_state[2][2]))
   
-draw_board()
+draw_board(board_state)
 
 def player_choice():
 
@@ -22,7 +26,7 @@ def player_choice():
     return player_input
 
 first_or_second = player_choice()           #Tracks if player is X's or O's
-board_state = [[' '] * 3 for _ in range(3)] #Track board state, initialized empty
+
 
 def three_in_a_row(X_or_O):
 
@@ -44,3 +48,29 @@ def three_in_a_row(X_or_O):
         return True
     
     return False
+
+player_turn = 'X'
+
+def is_legal_move(board_state, row, col):
+
+    #Determines if move is legal, based on whether the player has chosen
+    #an empty square
+
+    return -1 < row < 3 and -1 < col < 3 and board_state[row][col] == '_'
+
+def next_move(board_state, turn, X_or_O):
+
+    #Main game driver
+
+    row, col = 3, 3
+
+    if turn == X_or_O:          #Ask player for their move if it's their turn
+        while not is_legal_move(board_state, row, col):
+            row = int(input("Please input row"))
+            col = int(input("Please input column"))
+
+    print(row, col)
+    board_state[row][col] = X_or_O
+    draw_board(board_state)
+
+next_move(board_state, player_turn, first_or_second)
