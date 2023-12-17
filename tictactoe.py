e36f1ef0d99ed32942dcf77aab4f5a25e623f3d2
JeffKolnick <kolnickjeff@gmail.com>
2023-12-17 09:20:50 -0500

    Initial AI logic implemented; will play in centre square if available and will check if hypothetical move will win and play if so.

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+import copy
 
 board_state = [['_'] * 3 for _ in range(3)] #Track board state, initialized empty
 
@@ -28,7 +29,7 @@ def player_choice():
 first_or_second = player_choice()           #Tracks if player is X's or O's
 
 
-def three_in_a_row(X_or_O):
+def three_in_a_row(board_state, X_or_O):
 
     #Checks each row, column and diagonal for three of the same symbol,
     #which is the win condition and returns True if so
@@ -61,16 +62,47 @@ def is_legal_move(board_state, row, col):
 def next_move(board_state, turn, X_or_O):
 
     #Main game driver
-
+    
     row, col = 3, 3
 
     if turn == X_or_O:          #Ask player for their move if it's their turn
         while not is_legal_move(board_state, row, col):
             row = int(input("Please input row"))
             col = int(input("Please input column"))
+        
+        board_state[row][col] = X_or_O
 
-    print(row, col)
-    board_state[row][col] = X_or_O
+    else:
+        if board_state[1][1] == '_':    #If the centre is not taken, taking it is the best move
+            board_state[1][1] = X_or_O
+        
+        else:
+            legal_moves = []            #Determine remaining legal moves to see if there are game
+            for row in range(3):     #ending moves and react accordingly
+                for col in range(3):
+                    if is_legal_move(board_state, row ,col):
+                        legal_moves.append([row, col])
+
+            for move in legal_moves:
+                temp_board_state = copy.deepcopy(board_state)  #Create temporary board_state to check for potential
+                temp_board_state[move[0]][move[1]] = X_or_O #winning moves
+                if three_in_a_row(temp_board_state, X_or_O):
+                    board_state[move[0]][move[1]] = X_or_O
+
     draw_board(board_state)
+    
+    if three_in_a_row(board_state, X_or_O):              #Check if anyone wins
+        print("{} wins!".format(X_or_O))
+        return False
 
-next_move(board_state, player_turn, first_or_second)
+    if X_or_O == 'X':                         #Switch current player
+        return 'O'
+    else:
+        return 'X'
+
+board_state[1][0] = 'O'
+
+while True:
+    first_or_second = next_move(board_state, player_turn, first_or_second)
+    if not first_or_second:
+        break
